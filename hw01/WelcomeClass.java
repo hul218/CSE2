9b9b51d4ca381675f1bcdc20dc7232b5e955459e
Huanlun Li <hul218@lehigh.edu>
2014-08-29 23:21:18 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,16 @@
+//define the class
+public class WelcomeClass {
+//add main method
+  public static void main(String[] args ) {
+    System.out.println ("    -----------    ");
+    System.out.println ("    | WELCOME |    ");
+    System.out.println ("    -----------    ");
+    System.out.println ("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println (" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println ("|-H--U--L--2--1--8-|");
+    System.out.println (" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println ("  v  v  v  v  v  v  ");
+    System.out.println ("I am Huanlun Li, my nickname is Leo which is given by");
+    System.out.println ("my first English teacher. This Saturday, I have to take 1h bus travel to AT&T to reset my account.");
+  }
+}
