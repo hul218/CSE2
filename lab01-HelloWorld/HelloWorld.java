c77270cdf9d096c9d7767965f36574c4ae98f8b9
Huanlun Li <hul218@lehigh.edu>
2014-08-29 13:44:38 +0000

    Updated HelloWorld

--- lab01-HelloWorld/HelloWorld.java
+++ lab01-HelloWorld/HelloWorld.java
@@ -1,8 +1,12 @@
 //define the class
 public class HelloWorld {
-
+//add main method
   public static void main(String[] args) {
-      System.out.println("Hello World 2018!");
+      System.out.println("#######################");
+      System.out.println("#######################");
+      System.out.println("#  Hello World 2018!  #");
+      System.out.println("#######################");
+      System.out.println("#######################");
       
   }
 }
