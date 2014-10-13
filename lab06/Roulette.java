57cc620451d11ba484ac6d12f24f468d1463132d
Huanlun Li <hul218@lehigh.edu>
2014-10-13 02:07:56 +0000

    Updated lab06

--- lab06/Roulette.java
+++ lab06/Roulette.java
@@ -1,18 +1,14 @@
 //Huanlun Li [CSE02-110] Roulette.java 10/12/2014
-import java.util.Scanner;
 public class Roulette {
     public static void main(String[] args) {
-        Scanner MS=new Scanner( System.in );
-        System.out.print("Enter an int between 1 and 38 as the number you bet: ");
-        if (MS.hasNextInt()) { //ensure number entered is an int
-            int Bet=MS.nextInt();
-            if (Bet>=1&&Bet<=38) { //ensure number entered is in range
+                
                 int N=0; //N is the number of repetitions of 100 spins
                 int win=0; //win is the number of repetitions of 3 winnings 
                 int lose=0; //lose is the number of repetitions with no winning
                 int walkAway=0; //walkAway is the number of repetitions with profit( 0<winning<3 ) but walk away
                 while (N<1000) { //loop 1000 repetitions
                     int n=0;
+                    int Bet=(int) ((Math.random()*38)+1);
                     int correct=0; //correct is the number of winning of 1 spin
                     while (n<100) { //loop 100 spins
                         int RN=(int) ((Math.random()*38)+1); //RN is random number between 1 and 38
@@ -39,13 +35,6 @@ public class Roulette {
                        break;
                     }
                 }
-            }
-            else {
-                System.out.println("You did not enter a number in range.");
-            }
-        }
-        else {
-            System.out.println("You did not enter an int.");
-        }
+        
     }
 }
